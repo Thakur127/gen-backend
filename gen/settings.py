52c1fc8cf196d598ae8936eb7217b7f975ac7c68
Ravi Thakur <phenominal9159@gmail.com>
2023-08-23 19:00:07 +0530

    change debug option

--- gen/settings.py
+++ gen/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG") if not env("DEBUG") else True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", '.now.sh']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", ".now.sh"]
 
 
 # Application definition
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static') 
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 
 # Default primary key field type
