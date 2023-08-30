a03dda12e3f2401407e94212d60f9aca39c4a525
Ravi Thakur <phenominal9159@gmail.com>
2023-08-30 14:44:07 +0530

    changed database config

--- gen/settings.py
+++ gen/settings.py
@@ -101,20 +101,20 @@ WSGI_APPLICATION = "gen.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": env("PG_NAME"),
-#         "USER": env("PG_USER"),
-#         "PASSWORD": env("PG_PASSWORD"),
-#         "HOST": env("PG_HOST"),
-#         "PORT": env("PG_PORT"),
-#     }
-# }
-
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("PG_NAME"),
+        "USER": env("PG_USER"),
+        "PASSWORD": env("PG_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+    }
 }
+
+# DATABASES = {
+#     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
